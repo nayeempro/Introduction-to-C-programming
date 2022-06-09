0aaeab6249e26160b9998bce9240df3694a98160
Md Nayeem <89269773+nayeempro@users.noreply.github.com>
2022-06-09 16:41:26 +0600

    Add files via upload

---
+++ lec_01(array).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+//    int ar[5];
+//    printf("%d\n",&ar[0]);
+//    printf("%d\n",&ar[1]);
+//    printf("%d\n",&ar[2]);        //4 byte index increase
+//    printf("%d\n",&ar[3]);
+//    printf("%d\n",&ar[4]);
+
+
+
+//    long long int ar[5];
+//    printf("%d\n",&ar[0]);
+//    printf("%d\n",&ar[1]);
+//    printf("%d\n",&ar[2]);          //8 byte index increase
+//    printf("%d\n",&ar[3]);
+//    printf("%d\n",&ar[4]);
+
+
+
+
+
+
+
+
+return 0;
+}
